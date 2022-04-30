b16d01e5e8934dcd4c58083201b8849120a31607
WundoR <wunder@testchamber.one>
2022-04-30 21:51:41 +0300

    update

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -85,11 +85,11 @@
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
-       ;;ansible
+       ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
+       docker
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
