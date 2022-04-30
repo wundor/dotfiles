30d0c1d0d069b8c5c9da70cd09086f84e3938c5e
WundoR <wunder@testchamber.one>
2022-04-30 19:06:40 +0300

    iosevka for terminal

--- .bin/dotfiles-install.sh
+++ .bin/dotfiles-install.sh
@@ -10,7 +10,7 @@ if [ $? = 0 ]; then
   echo "Checked out dotfiles.";
   else
     echo "Backing up pre-existing dotfiles.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} sh -c 'rsync --remove-source-files --mkpath {} .dotfiles-backup/{} || rsync --remove-source-files {} .dotfiles-backup/{}'
 fi;
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
